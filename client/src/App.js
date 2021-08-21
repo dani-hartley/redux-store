a4090e269ba9679d98ebe544cf75ada1a54f4518
dhartley <dani_hartley@msn.com>
2021-08-20 21:33:07 -0500

    update store in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,11 @@ import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
+import { Provider } from 'react-redux';
+import configureStore from './utils/store';
+
+const store = configureStore();
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -42,7 +46,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store ={store}>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -53,7 +57,7 @@ function App() {
             <Route exact path="/success" component={Success} />
             <Route component={NoMatch} />
           </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
